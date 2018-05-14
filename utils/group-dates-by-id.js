9ff610bdcf30b74819e698ef32580cebb6776869
Mike Axtman <mike.axtman@nordstrom.com>
2018-05-14 16:43:25 -0700

    typo

--- utils/group-dates-by-id.js
+++ utils/group-dates-by-id.js
@@ -1,6 +1,6 @@
 const _ = require("lodash");
 
-const groupDatesById = (resuls) =>  _(resuls)
+const groupDatesById = (results) =>  _(results)
     .groupBy((resultObject) => resultObject.property_id.N)
     .mapValues((datesByProperty) => datesByProperty.map((property) => property.date.S))
     .value();
