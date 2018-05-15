2929b947bde48874074fce53fde8d88fe53d8f16
Mike Axtman <mike.axtman@nordstrom.com>
2018-05-15 15:35:58 -0700

    add lodash assign

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const createAvailability = require("./create-availabilities");
 const getAvailability = require("./get-availabilities");
 const deleteAvailability = require("./delete-availabilities");
+const _ = require("lodash");
 
-module.exports = {
-    ...deleteAvailability,
-    ...createAvailability,
-    ...getAvailability
-};
+module.exports = _.assign(
+    {},
+    deleteAvailability,
+    createAvailability,
+    getAvailability)
+    ;
